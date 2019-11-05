2dd94fb3cbc90cf1cd50ab5692fe79638df53ab1
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-11-05 10:24:37 -0800

    refactored the FolderName to show up with context

--- src/FolderName/FolderName.js
+++ src/FolderName/FolderName.js
@@ -1,29 +1,42 @@
 import React from 'react'
+import ApiContext from '../ApiContext'
 import './FolderName.css'
 
-function FolderName(props){
-    return (
-        <div className="FolderName">
-            <button
-                className="goBack-button"
-                onClick={() => props.history.goBack()}
-            >
-                Back
-            </button>
+class FolderName extends React.Component {
+    static defaultProps = {
+        history: {
+            goBack: () => {}
+        },
+        match: {
+            params: {}
+        }
+    }
+    static contextType = ApiContext;
 
-            {props.folder && (
-                <h3 className="FolderName__name">
-                    {props.folder.name}
-                </h3>
-            )}
+    render() {
+        console.log(this.context);
+        const { notes, folders } = this.context;
+        const { noteId } = this.props.match.params;
+        const note = notes.find( note => note.id === noteId);
+        const folder = folders.find( folder => folder.id === note.folderId);
 
-        </div>
-    )
-}
-
-FolderName.defaultProps = {
-    history: {
-        goBack: () => {}
+        return (
+            <div className="FolderName">
+                <button
+                    className="goBack-button"
+                    onClick={() => this.props.history.goBack()}
+                >
+                    Back
+                </button>
+    
+                {folder && (
+                    <h3 className="FolderName__name">
+                        {folder.name}
+                    </h3>
+                )}
+    
+            </div>
+        )
     }
 }
 
